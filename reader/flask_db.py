63396f71fd2d6a8c3634d606d72316607912ee69
Ben McKinley <bmckinley@fanatics.com>
2019-03-12 21:45:37 -0700

    a swing at flask_sqlalehemy

--- reader/flask_db.py
+++ reader/flask_db.py
@@ -2,12 +2,14 @@ import sqlite3
 
 from flask import current_app, g
 from flask.cli import with_appcontext
+from flask_sqlalchemy import SQLAlchemy
 
 
 def get_db():
     if 'db' not in g:
-        g.db = sqlite3.connect(current_app.config['DATABASE'])
-        g.db.row_factory = sqlite3.Row
+        g.db = SQLAlchemy(current_app)
+#        g.db = sqlite3.connect(current_app.config['DATABASE'])
+#        g.db.row_factory = sqlite3.Row
 
     return g.db
 
@@ -15,8 +17,8 @@ def get_db():
 def close_db(e=None):
     db = g.pop('db', None)
 
-    if db is not None:
-        db.close()
+    if db is not None and db.session is not None:
+        db.session.close()
 
 
 def init_app(app):
