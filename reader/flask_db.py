3cd6ded75d10996ab92ce20e3eafbce9ea3fc83d
joyfulflyer <benmckinley@gmail.com>
2019-03-12 22:31:42 -0700

    actually getting it working

--- reader/flask_db.py
+++ reader/flask_db.py
@@ -7,9 +7,9 @@ from flask_sqlalchemy import SQLAlchemy
 
 def get_db():
     if 'db' not in g:
-        g.db = SQLAlchemy(current_app)
-#        g.db = sqlite3.connect(current_app.config['DATABASE'])
-#        g.db.row_factory = sqlite3.Row
+#        g.db = SQLAlchemy(current_app)
+        g.db = sqlite3.connect(current_app.config['DATABASE'])
+        g.db.row_factory = sqlite3.Row
 
     return g.db
 
@@ -17,9 +17,11 @@ def get_db():
 def close_db(e=None):
     db = g.pop('db', None)
 
-    if db is not None and db.session is not None:
-        db.session.close()
-
+    try:
+        if db is not None and db.session is not None:
+            db.session.close()
+    except AttributeError:
+        db.close()
 
 def init_app(app):
     app.teardown_appcontext(close_db)
