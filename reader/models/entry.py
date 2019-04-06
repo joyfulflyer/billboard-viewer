c315c654e0764ef558e8d7dbc7095d2b160e139a
joyfulflyer <benmckinley@gmail.com>
2019-04-06 10:42:53 -0700

    update models

--- reader/models/entry.py
+++ reader/models/entry.py
@@ -13,6 +13,8 @@ class Entry(Base):
     last_position = Column(Integer, nullable=True)
     weeks_on_chart = Column(Integer, nullable=True)
     chart_id = Column(Integer, ForeignKey("charts.id"), nullable=False)
+    song_id = Column(Integer, ForeignKey("songs.id"), nullable=True)
 
     def __repr__(self):
-        return "Entry: <id='%r', name='%r', artist='%r'>" % (self.id, self.name, self.artist)
+        return "Entry: <id='%r', name='%r', artist='%r'>" % \
+            (self.id, self.name, self.artist)
--- reader/models/song.py
+++ reader/models/song.py
@@ -1,13 +1,17 @@
 from . base import Base
-from sqlalchemy import Integer, Column, String
+from sqlalchemy import Integer, Column, String, ForeignKey
 
 
 class Song(Base):
     __tablename__ = 'songs'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String)
-    artist = Column(String)
+    name = Column(String(256), nullable=False)
+    artist = Column(String(128), nullable=False)
+    spotify_id = Column(Integer, nullable=True)
+    search_term = Column(String(256), nullable=True)
+    search_results = Column(Integer, nullable=True)
 
     def __repr__(self):
-        return "Song: <id='%r', name='%r', artist='%r'>" % (self.id, self.name, self.artist)
+        return "Song: <id=%r, name=%r, spotify_id=%r>" % \
+            (self.id, self.name, self.spotify_id)
