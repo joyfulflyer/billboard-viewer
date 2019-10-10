a2018cee7009397e4373b8f1b08e4d70c7fcde8a
Ben McKinley <joyfulflyer@gmail.com>
2019-10-09 20:46:18 -0700

    work on env

--- config.py
+++ config.py
@@ -2,10 +2,8 @@ from os import environ, path
 import urllib.parse
 import logging
 
-
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-
 
 
 def get_db_type_from_variable():
@@ -23,27 +21,21 @@ def create_url_from_parts(username, password, host, dbname):
         url = base + db_path
     elif "mysql" in base:
         password = urllib.parse.quote_plus(password)
-        url = "%s%s:%s@%s/%s" % (base,
-                                 username,
-                                 password,
-                                 host,
-                                 dbname)
-    print("DB url: %r" % (url,))
+        url = "%s%s:%s@%s/%s" % (base, username, password, host, dbname)
+    print("DB url: %r" % (url, ))
     return url
 
 
 class Config:
-    username = "read_billboard" # environ['USERNAME']
-    password = "#23Kpq$M4Tnwd%PJMve"
+    username = environ['DB_USERNAME']
+    password = environ['PASS']
     host = environ.get('DB_HOST')
     db_name = environ.get('DATABASE')
-    SECRET_KEY='u9DCvDN82*$^!xbH#UG'
-    SQLALCHEMY_TRACK_MODIFICATIONS=False
-    SQLALCHEMY_DATABASE_URI=create_url_from_parts(username,
-                                                  password,
-                                                  host,
-                                                  db_name)
+    SECRET_KEY = 'u9DCvDN82*$^!xbH#UG'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = create_url_from_parts(username, password, host,
+                                                    db_name)
 
 
 class TestConfig(Config):
-    ENV="development"
+    ENV = "development"
