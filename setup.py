9f68972c3d2787bdb37dfb5305cd8ea65109d8f3
Ben McKinley <1526042+joyfulflyer@users.noreply.github.com>
2019-10-19 10:56:14 -0700

    update version number

--- setup.py
+++ setup.py
@@ -1,17 +1,10 @@
 from setuptools import setup, find_packages
 from os import path
 
-setup(
-    name="billboard-viewer",
-    version="0.1.0",
-    description="View the charts retrieved by the grabber",
-    author="Joyfulflyer",
-    packages=find_packages(),
-    python_requires='>=3.6, <4',
-    install_requires=[
-        'Flask',
-        'Flask-SQLAlchemy',
-        'PyMySQL',
-        'Flask-WTF'
-    ]
-)
+setup(name="billboard-viewer",
+      version="0.2.0",
+      description="View the charts retrieved by the grabber",
+      author="Joyfulflyer",
+      packages=find_packages(),
+      python_requires='>=3.6, <4',
+      install_requires=['Flask', 'Flask-SQLAlchemy', 'PyMySQL', 'Flask-WTF'])
