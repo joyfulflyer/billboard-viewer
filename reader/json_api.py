bf15c08f2e0f899c5d3708839231726f757b9586
Ben McKinley <1526042+joyfulflyer@users.noreply.github.com>
2019-12-08 20:11:19 -0800

    refactor json api

--- reader/json_api.py
+++ reader/json_api.py
@@ -47,9 +47,7 @@ General contract:
     if selected_id is None and request.is_json:
         selected_id = request.json.id
     song = _get_song_from_id(selected_id)
-    entries = _get_entries_from_song(song)
-    charts = list(map(convert_entry_to_chart, entries))
-    charts.sort(key=lambda chart: (chart["chartName"], chart["date"]))
+    charts = _get_sorted_charts(song)
     songDict = {
         "name": song.name,
         "artist": song.artist,
@@ -84,10 +82,8 @@ def song_by_id_with_sub_chart_entries(selected_id):
     }
     '''
     song = _get_song_from_id(selected_id)
-    entries = _get_entries_from_song(song)
-    charts = list(map(convert_entry_to_chart, entries))
-    charts.sort(key=lambda chart: (chart["chartName"], chart["date"]))
-    chartsWithEntries = list(map(add_entries_to_chart, charts))
+    charts = _get_sorted_charts(song)
+    chartsWithEntries = list(map(_add_entries_to_chart, charts))
     songDict = {
         "name": song.name,
         "artist": song.artist,
@@ -97,10 +93,8 @@ def song_by_id_with_sub_chart_entries(selected_id):
     return jsonify(songDict)
 
 
-def add_entries_to_chart(chart):
-    chart_entries = get_db().query(Entry) \
-        .filter_by(chart_id=chart['chartId']) \
-        .all()
+def _add_entries_to_chart(chart):
+    chart_entries = _get_chart_entries(chart['chartId'])
     chart_entries_mapped = map(
         lambda entry: {
             "name": entry.name,
@@ -112,7 +106,24 @@ def add_entries_to_chart(chart):
     return chart
 
 
-def convert_entry_to_chart(entry):
+@bp.route('/chart/<int:selected_id>')
+@bp.route('/chart', methods=("POST", ))
+def get_songs_for_chart(selected_id):
+    if selected_id is None and request.is_json:
+        selected_id = request.json.id
+    chart_entries = _get_chart_entries(selected_id)
+    converted = list(
+        map(
+            lambda entry: {
+                "name": entry.name,
+                "artist": entry.artist,
+                "place": entry.place,
+                "songId": entry.song_id
+            }, chart_entries))
+    return jsonify(converted)
+
+
+def _convert_entry_to_chart(entry):
     returnChart = {"place": entry.place, "chartId": entry.chart_id}
     chart = get_db().query(Chart) \
         .filter_by(id=entry.chart_id).one()
@@ -123,23 +134,11 @@ def convert_entry_to_chart(entry):
     return returnChart
 
 
-@bp.route('/chart/<int:selected_id>')
-@bp.route('/chart', methods=("POST", ))
-def get_songs_for_chart(selected_id):
-    if selected_id is None and request.is_json:
-        selected_id = request.json.id
+def _get_chart_entries(chart_id):
     chart_entries = get_db().query(Entry) \
-        .filter_by(chart_id=selected_id) \
+        .filter_by(chart_id=chart_id) \
         .all()
-    converted = list(
-        map(
-            lambda entry: {
-                "name": entry.name,
-                "artist": entry.artist,
-                "place": entry.place,
-                "songId": entry.song_id
-            }, chart_entries))
-    return jsonify(converted)
+    return chart_entries
 
 
 def _get_song_from_id(id):
@@ -155,3 +154,10 @@ def _get_entries_from_song(song):
     entries = get_db().query(Entry) \
         .filter_by(song_id=song.id).all()
     return entries
+
+
+def _get_sorted_charts(song):
+    entries = _get_entries_from_song(song)
+    charts = list(map(_convert_entry_to_chart, entries))
+    charts.sort(key=lambda chart: (chart["chartName"], chart["date"]))
+    return charts
