874582321a0a8a6ef65616f88fc34af53bb2600d
Ben McKinley <1526042+joyfulflyer@users.noreply.github.com>
2020-02-09 14:15:00 -0800

    allow search host to be set from env

--- reader/elastic/elastic.py
+++ reader/elastic/elastic.py
@@ -4,18 +4,23 @@ import json
 s = requests.Session()
 
 DEFAULT_URL = "http://localhost:9200/"
+DEFAULT_HOST = "localhost"
+DEFAULT_PORT = "9200"
+DEFAULT_SCHEME = "http"
 SEARCH_PATH = "song/_search/"
 
-_url = DEFAULT_URL
+host = DEFAULT_HOST
+scheme = DEFAULT_SCHEME
+port = DEFAULT_PORT
 
 
-def set_url(url):
-    _url = url
+def create_url():
+    return f"{scheme}://{host}:{port}/"
 
 
 def search_for_song(query):
     data = {"query": {"match": {"name": {"query": query}}}}
-    return s.post(DEFAULT_URL + SEARCH_PATH, json=data).json()
+    return s.post(create_url() + SEARCH_PATH, json=data).json()
 
 
 def results_for_song_search(query):
--- reader/elastic_client.py
+++ reader/elastic_client.py
@@ -1,7 +1,19 @@
 from .elastic import elastic
 from . import elastic_converter
+from flask import current_app, has_app_context
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def get_songs_with_name(song_name):
+    verify_search_host_with_env()
     results = elastic.results_for_song_search(song_name)
     return elastic_converter.convert_elastic_results_to_songs(results)
+
+
+def verify_search_host_with_env():
+    if elastic.host != current_app.config['SEARCH_HOST']:
+        host = current_app.config['SEARCH_HOST']
+        logger.debug("setting elastic host to " + host)
+        elastic.host = host
